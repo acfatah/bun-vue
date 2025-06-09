9fa9c6bc265df8a7ebe6702d7f96aa90a3aaed92
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-09 14:57:01 +0800

    Add @antfu/eslint-config

---
+++ eslint.config.ts
@@ -0,0 +1,32 @@
+import antfu from '@antfu/eslint-config'
+import pluginVitest from '@vitest/eslint-plugin'
+
+export default antfu(
+  {
+    formatters: true,
+    vue: true,
+  },
+
+  {
+    rules: {
+      'sort-imports': 'off',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          partitionByNewLine: true,
+          newlinesBetween: 'ignore',
+        },
+      ],
+    },
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'logs'],
+  },
+
+  {
+    rules: pluginVitest.configs.recommended.rules,
+    files: ['src/**/__tests__/*', 'tests/**/*'],
+  },
+)
