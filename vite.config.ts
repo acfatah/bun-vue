9a19936521343aa40a49b0ecd4f9a976daabf0a6
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-10 12:11:39 +0800

    Add vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    svgLoader(),
   ],
 
   resolve: {
