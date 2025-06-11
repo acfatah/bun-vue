d9685cc9a0a57022c6bb60bfb4804bdf3d73bc28
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-11 16:00:03 +0800

    feat: ability to set page title using route meta.pageTitle

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // import BlankLayout from '@/layouts/BlankLayout.vue'
 
+const DEFAULT_PAGE_TITLE = 'Vite + Vue + TS'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
@@ -31,6 +33,13 @@ const router = createRouter({
       // },
     },
   ],
+})
+
+router.beforeEach((to, _from, next) => {
+  const pageTitle = to.meta.pageTitle
+
+  document.title = pageTitle ? pageTitle as string : DEFAULT_PAGE_TITLE
+  next()
 })
 
 export default router
