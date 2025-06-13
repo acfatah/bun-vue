cc9d305c662bede2d205d1ddb60c02f5485310bd
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-14 07:30:05 +0800

    Add Popover component

---
+++ src/components/ui/popover/Popover.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { PopoverRootEmits, PopoverRootProps } from 'reka-ui'
+import { PopoverRoot, useForwardPropsEmits } from 'reka-ui'
+import { ref } from 'vue'
+
+const props = defineProps<PopoverRootProps>()
+const emits = defineEmits<PopoverRootEmits>()
+const forwarded = useForwardPropsEmits(props, emits)
+const open = ref(false)
+
+function setOpen(value: boolean) {
+  open.value = value
+}
+</script>
+
+<template>
+  <PopoverRoot
+    v-bind="forwarded"
+    v-model:open="open"
+    data-slot="popover"
+  >
+    <slot v-bind="{ open, setOpen }" />
+  </PopoverRoot>
+</template>
