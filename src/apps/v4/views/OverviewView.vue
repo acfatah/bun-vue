34359b65123ecb2ef2069072f2d8fa83197ee68f
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-16 17:10:46 +0800

    Add AccordionDemo

--- src/apps/v4/views/OverviewView.vue
+++ src/apps/v4/views/OverviewView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-// import ComponentWrapper from '../components/ComponentWrapper.vue'
+import ComponentWrapper from '../components/ComponentWrapper.vue'
+
+import AccordionDemo from '../registry/examples/AccordionDemo.vue'
 </script>
 
 <template>
   <main class="flex flex-1 flex-col justify-center">
     <div class="@container grid flex-1 gap-4 p-4">
-      <!-- TBD V4 Components -->
+      <ComponentWrapper name="Accordion">
+        <div class="grid w-full max-w-xl gap-4">
+          <AccordionDemo />
+        </div>
+      </ComponentWrapper>
     </div>
   </main>
 </template>
