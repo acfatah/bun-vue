331f2a97b000b1f859fca33066036b48bf2ad027
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-18 07:53:00 +0800

    Add Drawer component

---
+++ src/components/ui/drawer/DrawerDescription.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { DrawerDescriptionProps } from 'vaul-vue'
+import type { HtmlHTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { DrawerDescription } from 'vaul-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<DrawerDescriptionProps & {
+  class?: HtmlHTMLAttributes['class']
+}>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <DrawerDescription
+    data-slot="drawer-description"
+    v-bind="delegatedProps"
+    :class="cn('text-sm text-muted-foreground', props.class)"
+  >
+    <slot />
+  </DrawerDescription>
+</template>
---
+++ src/components/ui/drawer/DrawerTitle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { DrawerTitleProps } from 'vaul-vue'
+import type { HtmlHTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { DrawerTitle } from 'vaul-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<DrawerTitleProps & {
+  class?: HtmlHTMLAttributes['class']
+}>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <DrawerTitle
+    data-slot="drawer-title"
+    v-bind="delegatedProps"
+    :class="cn(
+      'font-semibold text-foreground',
+      props.class,
+    )"
+  >
+    <slot />
+  </DrawerTitle>
+</template>
