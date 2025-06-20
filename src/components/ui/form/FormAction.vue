fb3c1f1c099c72cb3df6d54a4dcfe332a8f9ac13
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-20 11:16:43 +0800

    Add Form components

---
+++ src/components/ui/form/FormAction.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  justifyEnd?: boolean
+  class?: HTMLAttributes['class']
+}>()
+
+const delegatedProps = reactiveOmit(props, 'class', 'justifyEnd')
+</script>
+
+<template>
+  <div
+    v-bind="delegatedProps"
+    :class="cn(
+      'mt-6 flex gap-2',
+      [props.justifyEnd && 'flex-row-reverse'],
+      props.class,
+    )"
+  >
+    <slot />
+  </div>
+</template>
