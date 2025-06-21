fe805c62db8fc5bde8a241d5988a09b4039e328f
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-22 00:19:03 +0800

    style: update SelectTrigger width for consistency

--- src/components/ui/select/SelectTrigger.vue
+++ src/components/ui/select/SelectTrigger.vue
@@ -24,7 +24,7 @@ const forwardedProps = useForwardProps(delegatedProps)
     v-bind="forwardedProps"
     :class="cn(
       `
-        flex w-fit items-center justify-between gap-2 rounded-md border border-input bg-transparent
+        flex w-full items-center justify-between gap-2 rounded-md border border-input bg-transparent
         px-3 py-2 text-sm whitespace-nowrap shadow-xs transition-[color,box-shadow] outline-none
         focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50
         disabled:cursor-not-allowed disabled:opacity-50
