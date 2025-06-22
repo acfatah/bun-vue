ed22a33dd126f5d0438cd9b8e5a7f3e762336f62
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-22 13:48:57 +0800

    Add Command components

---
+++ src/components/ui/kbd/Kbd.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactiveOmit } from '@vueuse/core'
+import { Primitive } from 'reka-ui'
+import { cn } from '@/lib/utils'
+import { kbdVariants } from '.'
+
+const props = defineProps({
+  class: { type: null, required: false },
+  size: { type: null, required: false, default: 'sm' },
+})
+
+const delegatedProps = reactiveOmit(props, 'class', 'size')
+</script>
+
+<template>
+  <Primitive
+    v-bind="delegatedProps"
+    :class="cn(
+      kbdVariants({ size: props.size }),
+      props.class,
+    )"
+  >
+    <span data-slot="kbd-content">
+      <slot />
+    </span>
+  </Primitive>
+</template>
---
+++ src/components/ui/kbd/index.ts
@@ -0,0 +1,24 @@
+import { cva } from 'class-variance-authority'
+
+export { default as Kbd } from './Kbd.vue'
+
+export const kbdVariants = cva(
+  `
+    pointer-events-none inline-flex items-center gap-1 rounded border bg-muted px-1.5 font-mono
+    font-medium text-muted-foreground opacity-100 select-none
+  `,
+  {
+    variants: {
+      size: {
+        xs: 'text-xs',
+        sm: 'text-sm',
+        md: 'text-md',
+        lg: 'text-lg',
+      },
+    },
+
+    defaultVariants: {
+      size: 'md',
+    },
+  },
+)
