03e6fd2e4e694d8274c5a74875e2068efe5105fd
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-23 16:08:43 +0800

    Add Sheet components

---
+++ src/components/ui/sheet/SheetContent.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import type { DialogContentEmits, DialogContentProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { Icon } from '@iconify/vue'
+import { reactiveOmit } from '@vueuse/core'
+import {
+  DialogClose,
+  DialogContent,
+  DialogPortal,
+  useForwardPropsEmits,
+} from 'reka-ui'
+import { cn } from '@/lib/utils'
+import type { SheetVariants } from '.'
+import { sheetVariants } from '.'
+import SheetOverlay from './SheetOverlay.vue'
+
+interface SheetContentProps extends DialogContentProps {
+  class?: HTMLAttributes['class']
+  side?: SheetVariants['side']
+}
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const props = withDefaults(defineProps<SheetContentProps>(), {
+  side: 'right',
+})
+
+const emits = defineEmits<DialogContentEmits>()
+const delegatedProps = reactiveOmit(props, 'class', 'side')
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
+
+<template>
+  <DialogPortal>
+    <SheetOverlay />
+    <DialogContent
+      data-slot="sheet-content"
+      :class="cn(sheetVariants({ side }), props.class)"
+      v-bind="{ ...forwarded, ...$attrs }"
+    >
+      <slot />
+
+      <DialogClose
+        :class="cn(
+          `
+            absolute top-4 right-4 rounded-xs opacity-70 ring-offset-background transition-opacity
+            hover:opacity-100
+            focus:ring-2 focus:ring-ring focus:ring-offset-2 focus:outline-hidden
+            disabled:pointer-events-none
+            data-[state=open]:bg-secondary
+          `,
+        )"
+      >
+        <Icon icon="lucide:x" />
+        <span class="sr-only">Close</span>
+      </DialogClose>
+    </DialogContent>
+  </DialogPortal>
+</template>
