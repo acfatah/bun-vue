2a5364aad6a17e3b500322cafb3c1e557f240ad5
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-24 17:52:35 +0800

    Add Tooltip components

---
+++ src/components/ui/tooltip/Tooltip.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { TooltipRootEmits, TooltipRootProps } from 'reka-ui'
+import {
+  TooltipProvider,
+  TooltipRoot,
+  useForwardPropsEmits,
+} from 'reka-ui'
+
+const props = defineProps<TooltipRootProps>()
+const emits = defineEmits<TooltipRootEmits>()
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <TooltipProvider>
+    <TooltipRoot
+      data-slot="tooltip"
+      v-bind="forwarded"
+    >
+      <slot />
+    </TooltipRoot>
+  </TooltipProvider>
+</template>
---
+++ src/components/ui/tooltip/TooltipArrow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { TooltipArrow } from 'reka-ui'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <TooltipArrow
+    v-bind="delegatedProps"
+    :class="cn(
+      'z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-primary fill-primary',
+      props.class,
+    )"
+  >
+    <slot />
+  </TooltipArrow>
+</template>
