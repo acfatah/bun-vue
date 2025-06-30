e4e280f55cae14a342713593aaeca57e38205220
Achmad F. Ibrahim <acfatah@gmail.com>
2025-06-30 08:01:57 +0800

    Add Hint demo

--- src/apps/v4/registry/examples/TooltipDemo.vue
+++ src/apps/v4/registry/examples/TooltipDemo.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
+import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import {
+  Hint,
   Tooltip,
   TooltipContent,
   TooltipTrigger,
@@ -36,19 +38,37 @@ const SIDES = ['top', 'right', 'bottom', 'left'] as const
       </Tooltip>
     </div>
 
-    <Tooltip>
-      <TooltipTrigger as-child>
-        <Button variant="ghost" size="icon">
-          <Icon icon="lucide:info" />
-          <span class="sr-only">Info</span>
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent
-        class="md:max-w-sm"
-      >
-        To learn more about how this works, check out the docs. If you have
-        any questions, please reach out to us.
-      </TooltipContent>
-    </Tooltip>
+    <div class="flex w-full gap-2">
+      <Tooltip>
+        <TooltipTrigger as-child>
+          <Button variant="ghost" size="icon">
+            <Icon icon="lucide:info" />
+            <span class="sr-only">Info</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent
+          class="md:max-w-sm"
+        >
+          To learn more about how this works, check out the docs. If you have
+          any questions, please reach out to us.
+        </TooltipContent>
+      </Tooltip>
+    </div>
+
+    <div class="space-y-4">
+      <p>
+        Some text. <Hint text="Simple text hint">
+          <Icon icon="lucide:info" class="relative -top-[1.5px] inline" />
+        </Hint>
+      </p>
+
+      <p>
+        <Hint text="Simple text hint">
+          <Badge variant="success" class="select-none">
+            Example
+          </Badge>
+        </Hint>
+      </p>
+    </div>
   </div>
 </template>
