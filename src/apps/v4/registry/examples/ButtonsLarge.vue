a2bad69bb5baea86da752384a1cf98eccd688989
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-04 20:29:49 +0800

    chore: add new lines to improve readability

--- src/apps/v4/registry/examples/ButtonsLarge.vue
+++ src/apps/v4/registry/examples/ButtonsLarge.vue
@@ -11,32 +11,41 @@ import { Button } from '@/components/ui/button'
     "
   >
     <Button size="lg">
-      Small
+      Large
     </Button>
+
     <Button variant="secondary" size="lg">
       Secondary
     </Button>
+
     <Button variant="destructive" size="lg">
       Destructive
     </Button>
+
     <Button variant="outline" size="lg">
       Outline
     </Button>
+
     <Button variant="ghost" size="lg">
       Ghost
     </Button>
+
     <Button variant="link" size="lg">
       Link
     </Button>
+
     <Button variant="secondary" size="icon" class="size-8">
       <Icon icon="lucide:chevron-right" />
     </Button>
+
     <Button variant="outline" size="lg">
       <Icon icon="lucide:send" /> Send
     </Button>
+
     <Button variant="outline" size="lg">
       Learn More <Icon icon="lucide:arrow-right" />
     </Button>
+
     <Button disabled size="lg">
       <Icon icon="lucide:loader-2" class="animate-spin" />
       Please wait
