45ba6182fa499bf73300d646d1e0ef8e6f7d9184
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-25 16:06:18 +0800

    feat: add TagsInput component

---
+++ src/components/ui/tags-input/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Tags Input
+ *
+ * Tag inputs render tags inside an input, followed by an actual text input.
+ */
+
+export { default as TagsInput } from './TagsInput.vue'
+export { default as TagsInputInput } from './TagsInputInput.vue'
+export { default as TagsInputItem } from './TagsInputItem.vue'
+export { default as TagsInputItemDelete } from './TagsInputItemDelete.vue'
+export { default as TagsInputItemText } from './TagsInputItemText.vue'
