49ab951f1c0b0270db844151dc09d53095ca518b
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-25 16:05:26 +0800

    feat: add Stepper component

---
+++ src/components/ui/stepper/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Stepper
+ *
+ * A set of steps that are used to indicate progress through a multi-step process.
+ */
+
+export { default as Stepper } from './Stepper.vue'
+export { default as StepperDescription } from './StepperDescription.vue'
+export { default as StepperIndicator } from './StepperIndicator.vue'
+export { default as StepperItem } from './StepperItem.vue'
+export { default as StepperSeparator } from './StepperSeparator.vue'
+export { default as StepperTitle } from './StepperTitle.vue'
+export { default as StepperTrigger } from './StepperTrigger.vue'
