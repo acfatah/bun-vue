c2a4081f6138c34e88bf94d39a08847d2230a089
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-25 11:40:53 +0800

    feat: add NumberField component

---
+++ src/components/ui/number-field/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Number Field
+ *
+ * A number field allows a user to enter a number and increment or decrement the value using stepper buttons.
+ */
+
+export { default as NumberField } from './NumberField.vue'
+export { default as NumberFieldContent } from './NumberFieldContent.vue'
+export { default as NumberFieldDecrement } from './NumberFieldDecrement.vue'
+export { default as NumberFieldIncrement } from './NumberFieldIncrement.vue'
+export { default as NumberFieldInput } from './NumberFieldInput.vue'
