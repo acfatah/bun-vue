fd6de7c3aa651794917aa4efbb9259e3af10a2a5
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-26 23:12:20 +0800

    refactor(registry): rename component

--- src/registry/blocks/login-01/router/sign-in.ts
+++ src/registry/blocks/login-01/router/sign-in.ts
@@ -1,13 +1,13 @@
 import type { RouteRecordRaw } from 'vue-router'
 import CenteredLayout from '@/layouts/CenteredLayout.vue'
-import Login from '@/pages/SignIn.vue'
+import SignIn from '@/pages/SignIn.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/sign-in',
     name: 'sign-in',
     meta: { layout: CenteredLayout },
-    component: Login,
+    component: SignIn,
   },
 ]
 
