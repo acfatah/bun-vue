254a5516b3d543fc36c9becdb0ac22556c860f9d
Achmad F. Ibrahim <acfatah@gmail.com>
2025-08-07 10:56:39 +0800

    feat(template): add list, find and info command

--- templates/starter/scripts/ui.ts
+++ templates/starter/scripts/ui.ts
@@ -4,10 +4,73 @@ import { program } from 'commander'
 import { consola } from 'consola'
 import process from 'node:process'
 
-const REGISTRY_URL = process.env.REGISTRY_URL || 'http://localhost:8080'
+const REGISTRY_URL = process.env.REGISTRY_URL || 'http://localhost:8080/r'
+
+async function listComponents() {
+  const res = await fetch(`${REGISTRY_URL}/index.json`)
+
+  return await res.json()
+}
+
+program.command('list')
+  .description('List available blocks and components')
+  .action(async () => {
+    const components = await listComponents()
+
+    consola.log(components.reduce((acc: string, component: any) => {
+      acc += `${component.name} `
+
+      return acc
+    }, '').trim())
+  })
+
+program.command('info')
+  .description('Display information about a block or component')
+  .argument('<component>', 'the component to display information about')
+  .action(async (arg: string) => {
+    let res: Response
+
+    try {
+      res = await fetch(`${REGISTRY_URL}/${arg}.json`)
+    }
+    catch (error) {
+      consola.error(error)
+
+      process.exit(1)
+    }
+
+    if (res.status === 404) {
+      consola.error('Component not found')
+
+      process.exit(1)
+    }
+
+    const component = await res.json()
+
+    consola.log(JSON.stringify({
+      ...component,
+      // skip the content key
+      files: component.files.map(({ type, path }) => ({ type, path })),
+    }, null, 2))
+  })
+
+program.command('find')
+  .description('Find matching components')
+  .argument('<query>', 'the query to search for')
+  .action(async (arg: string) => {
+    const components = await listComponents()
+
+    consola.log(components.reduce((acc: string, component: any) => {
+      if (component.name.includes(arg)) {
+        acc += `${component.name} `
+      }
+
+      return acc
+    }, '').trim())
+  })
 
 program.command('add')
-  .description('add a component to your project')
+  .description('Add a component to your project')
   .argument('<components...>', 'the components to add')
   .option('--first', 'display just the first substring')
   .option('-y, --yes', 'skip confirmation prompt. (default: false)')
@@ -15,7 +78,7 @@ program.command('add')
   .option('-s, --silent', 'mute output. (default: false)')
   .action(async (components, options) => {
     const urls: string[] = components.reduce((acc: string[], component: string) => {
-      acc.push(`${REGISTRY_URL}/r/${component}.json`)
+      acc.push(`${REGISTRY_URL}/${component}.json`)
 
       return acc
     }, [])
