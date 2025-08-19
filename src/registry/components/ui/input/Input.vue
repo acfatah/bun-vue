72dd54ef19376c0dbbbd4e0a52d2838888d630a8
Achmad F. Ibrahim <acfatah@gmail.com>
2025-08-19 11:39:23 +0800

    refactor(ui): remove text-base class from default

--- src/registry/components/ui/input/Input.vue
+++ src/registry/components/ui/input/Input.vue
@@ -25,8 +25,8 @@ const modelValue = useVModel(props, 'modelValue', emits, {
     data-slot="input"
     :class="cn(
       `
-        flex h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 text-base
-        shadow-xs transition-[color,box-shadow] outline-none
+        flex h-9 w-full min-w-0 rounded-md border border-input bg-transparent px-3 py-1 shadow-xs
+        transition-[color,box-shadow] outline-none
         selection:bg-primary selection:text-primary-foreground
         file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium
         file:text-foreground
