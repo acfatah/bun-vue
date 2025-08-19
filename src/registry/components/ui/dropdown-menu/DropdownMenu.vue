49b981d9d9436b8d880e3d295a49f196ff6d2dd3
Achmad F. Ibrahim <acfatah@gmail.com>
2025-08-19 11:37:44 +0800

    refactor(ui): expose open and setOpen as slot props

--- src/registry/components/ui/dropdown-menu/DropdownMenu.vue
+++ src/registry/components/ui/dropdown-menu/DropdownMenu.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import type { DropdownMenuRootEmits, DropdownMenuRootProps } from 'reka-ui'
 import { DropdownMenuRoot, useForwardPropsEmits } from 'reka-ui'
+import { ref } from 'vue'
 
 const props = defineProps<DropdownMenuRootProps>()
 const emits = defineEmits<DropdownMenuRootEmits>()
 const forwarded = useForwardPropsEmits(props, emits)
+const open = ref(false)
+
+function setOpen(value: boolean) {
+  open.value = value
+}
 </script>
 
 <template>
   <DropdownMenuRoot
-    data-slot="dropdown-menu"
     v-bind="forwarded"
+    v-model:open="open"
+    data-slot="dropdown-menu"
   >
-    <slot />
+    <slot v-bind="{ open, setOpen }" />
   </DropdownMenuRoot>
 </template>
