65b81f3c7cd2920b09a966ef3ab3d6200f481b86
Achmad F. Ibrahim <acfatah@gmail.com>
2025-08-22 15:35:24 +0800

    refactor(ui): add max width and max height to DialogContent

--- src/registry/components/ui/dialog/DialogContent.vue
+++ src/registry/components/ui/dialog/DialogContent.vue
@@ -29,8 +29,9 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
       v-bind="forwarded"
       :class="cn(
         `
-          fixed top-1/2 left-1/2 z-50 grid w-full max-w-[calc(100%-2rem)] -translate-x-1/2
-          -translate-y-1/2 gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200
+          fixed top-1/2 left-1/2 z-50 grid max-h-[calc(100svh-2rem)] w-full
+          max-w-[calc(100svw-2rem)] -translate-x-1/2 -translate-y-1/2 gap-4 rounded-lg border
+          bg-background p-6 shadow-lg duration-200
           data-[state=closed]:animate-out data-[state=closed]:fade-out-0
           data-[state=closed]:zoom-out-95
           data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95
